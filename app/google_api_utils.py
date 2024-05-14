5117d6c17c3f1572ef28a1d403b84e2683668b72
Timur Baiguzhinov <tim.baiguzhinov@gmail.com>
2024-05-14 16:31:49 +0400

    added google api handler

---
+++ app/google_api_utils.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+from app.config import settings
+
+
+async def get_translation(word, target_language):
+    url = "https://translation.googleapis.com/language/translate/v2"
+    params = {
+        "q": word,
+        "target": target_language,
+        "key": settings.GOOGLE_API_KEY
+    }
+    try:
+        response = requests.get(url, params=params)
+        translation = response.text
+        return translation
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
