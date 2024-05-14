6274b4b16f98ddf3a4785c4dbf88319243c0220d
Timur Baiguzhinov <tim.baiguzhinov@gmail.com>
2024-05-14 16:32:20 +0400

    added db utils

---
+++ app/db_utils.py
@@ -0,0 +1,19 @@
+import certifi
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from app.config import settings
+
+
+client = MongoClient(
+    uri=settings.MONGODB_URL,
+    server_api=ServerApi('1'),
+    tlsCAFile=certifi.where()
+)
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
